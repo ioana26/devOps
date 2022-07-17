dedecb8eb439c58180546f76ad6e279cce48409c
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-17 21:48:15 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,12 @@
 
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "3.26.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
+    
   }
-  required_version = ">= 1.1.0"
 
   cloud {
     organization = "iavirvarei"
@@ -24,24 +19,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-}
-
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "web" {
