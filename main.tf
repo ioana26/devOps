3523f0ad1c253ff803cfebec6c39a1b934d7f63b
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-18 22:10:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
   cloud {
     organization = "iavirvarei"
   workspaces {
-      name = "Google_credentials"
+      name = "DevTest"
     }
   token = "cABF9S0BN7gaqQ.atlasv1.Xh43DVWXFiOUkmJlayYvirSXT3SuJvX1VNKFfUXq54tjulCnnibyxRrch4NsjexHtyE"
   }
