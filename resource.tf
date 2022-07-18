fa52d86baa785dbda2abe37424cd27a3783990e3
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-18 09:55:33 +0300

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,14 @@
+resource "google-compute_instace" "my-server" {
+  name = "my-gcp_server"
+  machine = "f1.micro"
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-g"
+    }
+  }
+  network_interface {
+    network = "default" //This Enable Private IP Addresses
+    access_config{}  //This Enable Public IP Address
+  }
+}
