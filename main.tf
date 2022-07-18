16be23c7e6dcbecc8425e7cfbfb91d9a38968630
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-18 15:17:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
-  
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = ">= 3.29"
     }
-    
-  cloud {
-    organization = "iavirvarei"
 
-    workspaces {
-      name = "Google_credentials"
+    cloud {
+      organization = "iavirvarei"
+
+      workspaces {
+        name = "Google_credentials"
+      }
     }
-  }
   }
 }
