74f0e42fc133d86c5d7bc3f35d52376bc1d6f647
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-18 11:46:06 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,24 +26,3 @@ resource "google-compute_instace" "my-server" {
   }
 }
 
-//resource "aws_instance" "web" {
-  //ami                    = data.aws_ami.ubuntu.id
-  //instance_type          = "t2.micro"
-  //vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-
-
-/*resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
-*/
